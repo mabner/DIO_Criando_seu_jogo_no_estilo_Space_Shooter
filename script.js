c85af92ac95cced76117bd7b725dc808eaacf31a
Marcos Leite <m.abner@live.co.uk>
2022-04-29 17:48:19 -0300

    createAliens function

--- script.js
+++ script.js
@@ -75,4 +75,16 @@ function moveLaser(laser) {
 	}, 10);
 }
 
+function createAliens() {
+	let newAlien = document.createElement('img');
+	let alienSprite = aliensImg[Math.floor(Math.random() * aliensImg.length)];
+	newAlien.src = alienSprite;
+	newAlien.classList.add('alien');
+	newAlien.classList.add('alien-transition');
+	newAlien.style.left = '370px';
+	newAlien.style.top = `${Math.floor(Math.random() * 330) + 30}px`;
+	playArea.appendChild(newAlien);
+	moveAlien(newAlien);
+}
+
 window.addEventListener('keydown', flyShip);
