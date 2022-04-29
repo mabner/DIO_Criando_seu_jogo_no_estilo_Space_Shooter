df5df1660a2e802d83eb3b05f86e4bd1371c04de
Marcos Leite <m.abner@live.co.uk>
2022-04-29 17:06:11 -0300

    laser shooting and animation

--- script.js
+++ script.js
@@ -27,10 +27,11 @@ function moveUp() {
 
 function moveDown() {
 	let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
-	if (topPosition === '500px') {
+	let position = parseInt(topPosition);
+
+	if (position >= 505) {
 		return;
 	} else {
-		let position = parseInt(topPosition);
 		position += 50;
 		yourShip.style.top = `${position}px`;
 	}
@@ -39,24 +40,34 @@ function moveDown() {
 function fireLaser() {
 	let laser = createLaserElement();
 	playArea.appendChild(laser);
-	moveLaser();
+	moveLaser(laser);
 }
 
 function createLaserElement() {
 	let xPosition = parseInt(
-		window.getComputedStyle(yourShip).getPropertyValue('left'),
+		window.getComputedStyle(yourShip).getPropertyValue('left')
 	);
 	let yPosition = parseInt(
-		window.getComputedStyle(yourShip).getPropertyValue('top'),
+		window.getComputedStyle(yourShip).getPropertyValue('top')
 	);
 	let newLaser = document.createElement('img'); // creates the img tag
 	newLaser.src = 'img/shoot.png';
 	newLaser.classList.add('laser');
-	newLaser.style.left = `$(xPosition)px`;
-	newLaser.style.top = `$(yPosition - 10)px`;
+	newLaser.style.left = `${xPosition}px`;
+	newLaser.style.top = `${yPosition - 10}px`;
 	return newLaser;
 }
 
-function moveLaser() { };
+function moveLaser(laser) {
+	let laserInterval = setInterval(() => {
+		let xPosition = parseInt(laser.style.left);
+
+		if (xPosition === 340) {
+			laser.remove();
+		} else {
+			laser.style.left = `${xPosition + 8}px`;
+		}
+	}, 10);
+}
 
 window.addEventListener('keydown', flyShip);
