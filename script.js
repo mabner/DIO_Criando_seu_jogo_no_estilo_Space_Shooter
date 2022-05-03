d35fae61e71e659d047ce4516cc6f3fe5161e37e
Marcos Leite <m.abner@live.co.uk>
2022-05-03 16:47:38 -0300

    moveAlien, check colision and alien death event

--- script.js
+++ script.js
@@ -66,6 +66,16 @@ function createLaserElement() {
 function moveLaser(laser) {
 	let laserInterval = setInterval(() => {
 		let xPosition = parseInt(laser.style.left);
+		let aliens = document.querySelectorAll('alien');
+
+		aliens.forEach((alien) => {
+			// loop repeats for each iteration in the alien list
+			if (checkLaserCollision(laser, alien)) {
+				alien.src = 'img/explosion.png';
+				alien.classList.remove('alien');
+				alien.classList.add('dead-alien');
+			}
+		});
 
 		if (xPosition === 340) {
 			laser.remove();
@@ -87,4 +97,42 @@ function createAliens() {
 	moveAlien(newAlien);
 }
 
+function moveAlien(alien) {
+	let moveAlienInterval = setInterval(() => {
+		let xPosition = parseInt(
+			window.getComputedStyle(alien).getPropertyValue('left')
+		);
+		if (xPosition <= 50) {
+			if (Array.from(alien.classList).includes('dead-alien')) {
+				alien.remove();
+			} else {
+				gameOver();
+			}
+		} else {
+			alien.style.left = `${xPosition - 4}px`;
+		}
+	}, 30);
+}
+
+function checkLaserCollision(laser, alien) {
+	let laserTop = parseInt(laser.style.top);
+	let laserLeft = parseInt(laser.style.left);
+	let laserBottom = laserTop - 20;
+
+	let alienTop = parseInt(alien.style.top);
+	let alienLeft = parseInt(alien.style.left);
+	let alienBottom = alienTop - 30;
+
+	if (laserLeft != 340 && laserLeft + 40 >= alienLeft) {
+		if (laserTop <= alienTop && laserTop >= alienBottom) {
+			return true;
+		} else {
+			return false;
+		}
+	} else {
+		return false;
+	}
+}
+
 window.addEventListener('keydown', flyShip);
+createAliens();
